1f74f86ae2b327b8ccd5ecea71e77e034ab46466
madzi <madzihove@gmail.com>
2026-04-07 19:28:50 +0200

    add loop to 'populate_arr'

--- allergies.c
+++ allergies.c
@@ -1,4 +1,5 @@
 #include "allergies.h"
+
 // check if allergy has been added before
 bool is_false(const allergen_list_t *allergen_list, const allergen_t allergen){
 	return allergen_list->allergens[allergen] == 0;
@@ -10,54 +11,56 @@ bool is_allergic_to(const allergen_t allergen, const uint16_t score) {
 }
 
 void populate_arr(allergen_list_t *allergens, uint16_t score){
-    if (score >= 128) {
-        score -= 128;
-		if (is_false(allergens, ALLERGEN_CATS)){
-            allergens->allergens[ALLERGEN_CATS] = 1;
-            allergens->count++;
+    while (score != 0) {
+        if (score >= 128) {
+            score -= 128;
+            if (is_false(allergens, ALLERGEN_CATS)){
+                allergens->allergens[ALLERGEN_CATS] = 1;
+                allergens->count++;
+            }
         }
-    }
-    else if (score >= 64) {
-        score -= 64;
-        if(is_false(allergens, ALLERGEN_POLLEN)){
-            allergens->allergens[ALLERGEN_POLLEN] = 1;
-            allergens->count++;
+        else if (score >= 64) {
+            score -= 64;
+            if(is_false(allergens, ALLERGEN_POLLEN)){
+                allergens->allergens[ALLERGEN_POLLEN] = 1;
+                allergens->count++;
+            }
         }
-    }
-    else if (score >= 32) {
-        score -= 32;
-        if(is_false(allergens, ALLERGEN_CHOCOLATE)){
-            allergens->allergens[ALLERGEN_CHOCOLATE] = 1;
-            allergens->count++;
+        else if (score >= 32) {
+            score -= 32;
+            if(is_false(allergens, ALLERGEN_CHOCOLATE)){
+                allergens->allergens[ALLERGEN_CHOCOLATE] = 1;
+                allergens->count++;
+            }
         }
-    }
-    else if (score >= 16) {
-        score -= 16;
-        if(is_false(allergens, ALLERGEN_TOMATOES)){
-            allergens->allergens[ALLERGEN_TOMATOES] = 1;
-            allergens->count++;
+        else if (score >= 16) {
+            score -= 16;
+            if(is_false(allergens, ALLERGEN_TOMATOES)){
+                allergens->allergens[ALLERGEN_TOMATOES] = 1;
+                allergens->count++;
+            }
         }
-    }
-    else if (score >= 8) {
-        score -= 8;
-        if(is_false(allergens, ALLERGEN_STRAWBERRIES)){
-            allergens->allergens[ALLERGEN_STRAWBERRIES] = 1;
-            allergens->count++;
+        else if (score >= 8) {
+            score -= 8;
+            if(is_false(allergens, ALLERGEN_STRAWBERRIES)){
+                allergens->allergens[ALLERGEN_STRAWBERRIES] = 1;
+                allergens->count++;
+            }
         }
-    }
-    else if (score >= 4) {
-        score -= 4;
-        if(is_false(allergens, ALLERGEN_SHELLFISH)){
-            allergens->allergens[ALLERGEN_SHELLFISH] = 1;
-            allergens->count++;
+        else if (score >= 4) {
+            score -= 4;
+            if(is_false(allergens, ALLERGEN_SHELLFISH)){
+                allergens->allergens[ALLERGEN_SHELLFISH] = 1;
+                allergens->count++;
+            }
         }
-    }
-    else {
-        score -= 2;
-        if(is_false(allergens, ALLERGEN_PEANUTS)){
-            allergens->allergens[ALLERGEN_PEANUTS] = 1;
-            allergens->count++;
+        else {
+            score -= 2;
+            if(is_false(allergens, ALLERGEN_PEANUTS)){
+                allergens->allergens[ALLERGEN_PEANUTS] = 1;
+                allergens->count++;
 
+            }
         }
     }
 }
@@ -73,8 +76,14 @@ allergen_list_t get_allergens(const uint16_t score) {
             temp.count++;
         }
     }
-while (tmp_score != 0) {
-    // still to write
-}
+
+    
+    if (tmp_score > 128 && (tmp_score % 128) == 0) // this condition
+        return temp;
+    else if (tmp_score >= 128 && (tmp_score % 12) != 0)
+        populate_arr(&temp, tmp_score);
+    else
+        populate_arr(&temp, tmp_score);
+
     return temp;
 }
